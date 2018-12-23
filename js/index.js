a0e337ae7edabe1ba42550744c988275473b818c
huxintao <huxt3@asiainfo.com>
2018-12-23 16:11:59 +0800

    test

--- js/index.js
+++ js/index.js
@@ -1,17 +1,16 @@
+var userInfo = Util.getUserInfo();
+
 function search() {
     if ($("#search_input").val() != "") {
         // window.location.href = "https://www.baidu.com/s?word=" + document.getElementById("search_input").value;
         window.open("https://www.baidu.com/s?word=" + $("#search_input").val());
-
         $("#search_input").val("");
     }
     return false;
 }
 
-var userInfo = Util.getUserInfo();
 
 function getIndex() {
-
     var param = {
         userid: userInfo.id
     };
@@ -20,18 +19,14 @@ function getIndex() {
             alert(response.message);
             return;
         }
-
-        result = response.result;
-        $.each(result, function (i, v) {
+        $.each(response.result, function (i, v) {
             $('#copy a').attr('href', v.menuUrl);
             $('#copy img').attr('src', v.menuImgUrl);
             $('#copy p:eq(1)').html(v.menuName);
             $('.boxs').append($('#copy').html());
         });
-
     });
 }
-
 
 $(function () {
     if (userInfo) {
