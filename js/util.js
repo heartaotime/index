9bd6e0f5a505f57620522bfe4d55db621daebeb3
huxintao <huxt3@asiainfo.com>
2019-01-17 10:53:37 +0800

    test

--- js/util.js
+++ js/util.js
@@ -94,62 +94,62 @@ window.Util = (function () {
     }
 
     var getUserIP = function (onNewIP) {
-        //compatibility for firefox and chrome
-        var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-        var pc = new myPeerConnection({
-                iceServers: []
-            }),
-            noop = function () {
-            },
-            localIPs = {},
-            ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
-            key;
+        try {
+            //compatibility for firefox and chrome
+            var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+            var pc = new myPeerConnection({
+                    iceServers: []
+                }),
+                noop = function () {
+                },
+                localIPs = {},
+                ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+                key;
 
-        function iterateIP(ip) {
-            if (!localIPs[ip]) onNewIP(ip);
-            localIPs[ip] = true;
-        }
+            function iterateIP(ip) {
+                if (!localIPs[ip]) onNewIP(ip);
+                localIPs[ip] = true;
+            }
 
-        //create a bogus data channel
-        pc.createDataChannel("");
+            //create a bogus data channel
+            pc.createDataChannel("");
 
-        // create offer and set local description
-        pc.createOffer().then(function (sdp) {
-            sdp.sdp.split('\n').forEach(function (line) {
-                if (line.indexOf('candidate') < 0) return;
-                line.match(ipRegex).forEach(iterateIP);
+            // create offer and set local description
+            pc.createOffer().then(function (sdp) {
+                sdp.sdp.split('\n').forEach(function (line) {
+                    if (line.indexOf('candidate') < 0) return;
+                    line.match(ipRegex).forEach(iterateIP);
+                });
+
+                pc.setLocalDescription(sdp, noop, noop);
+            }).catch(function (reason) {
+                // An error occurred, so handle the failure to connect
             });
 
-            pc.setLocalDescription(sdp, noop, noop);
-        }).catch(function (reason) {
-            // An error occurred, so handle the failure to connect
-        });
+            //sten for candidate events
+            pc.onicecandidate = function (ice) {
+                if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+                ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+            };
+        } catch (e) {
+            onNewIP('127.0.0.1'); // 获取ip失败
+        }
 
-        //sten for candidate events
-        pc.onicecandidate = function (ice) {
-            if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
-            ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
-        };
     }
 
     var statistics = function (pageinfo) {
         var userInfo = getUserInfo();
-        try {
-            getUserIP(function (ip) {
-                var param = {
-                    userid: userInfo ? userInfo.id : -1,
-                    clientip: ip,
-                    pageinfo: pageinfo,
-                    remark: ''
-                };
-                // 统计信息
-                Util.postJson("./common-server/user/api/v1/statistics", param, function (response) {
-                });
+        getUserIP(function (ip) {
+            var param = {
+                userid: userInfo ? userInfo.id : -1,
+                clientip: ip,
+                pageinfo: pageinfo,
+                remark: ''
+            };
+            // 统计信息
+            Util.postJson("./common-server/user/api/v1/statistics", param, function (response) {
             });
-        } catch (error) {
-            console.log('Util.statistics error,', error);
-        }
-
+        });
     }
 
 
