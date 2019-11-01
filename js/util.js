c31a4a69a948710d6e0562945f0e0136d4059e76
huxintao <huxt3@asiainfo.com>
2019-11-01 21:02:36 +0800

    grid布局版本

--- js/util.js
+++ js/util.js
@@ -1,7 +1,7 @@
 window.Util = (function () {
 
     // 引入jquery
-    document.write('<script src = "https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>');
+    document.write('<script src = "https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js"></script>');
     // document.write('<script src = "http://pv.sohu.com/cityjson?ie=utf-8"></script>');
     // document.write('<script src = "https://code.jquery.com/jquery-3.3.1.min.js"></script>');
     // document.write('<script src = "https://cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>');
